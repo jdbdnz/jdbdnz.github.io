c57f0975a89305ddae7c16b66daae8ec9e2f507e
Josh Dean <jdbdean@gmail.com>
2015-01-11 14:52:55 +1300

    typo

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,16 +1,16 @@
 # Set the working application directory
 # working_directory "/path/to/your/app"
-working_directory "/homes/groot/apps/jdbd"
+working_directory "/home/groot/apps/jdbd"
 
 # Unicorn PID file location
 # pid "/path/to/pids/unicorn.pid"
-pid "/homes/groot/apps/jdbd/pids/unicorn.pid"
+pid "/home/groot/apps/jdbd/pids/unicorn.pid"
 
 # Path to logs
 # stderr_path "/path/to/log/unicorn.log"
 # stdout_path "/path/to/log/unicorn.log"
-stderr_path "/homes/groot/apps/jdbd/log/unicorn.log"
-stdout_path "/homes/groot/apps/jdbd/log/unicorn.log"
+stderr_path "/home/groot/apps/jdbd/log/unicorn.log"
+stdout_path "/home/groot/apps/jdbd/log/unicorn.log"
 
 # Unicorn socket
 listen "/tmp/unicorn.[app name].sock"
