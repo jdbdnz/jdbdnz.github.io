8eebbe38ac4f3217c42fb0c5a5af314ce8424fb6
Josh Dean <jdbdean@gmail.com>
2015-01-11 14:51:46 +1300

    unicorn

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,22 +1,24 @@
-worker_processes Integer(ENV["WEB_CONCURRENCY"] || 3)
-timeout 15
-preload_app true
+# Set the working application directory
+# working_directory "/path/to/your/app"
+working_directory "/homes/groot/apps/jdbd"
 
-before_fork do |server, worker|
-  Signal.trap 'TERM' do
-    puts 'Unicorn master intercepting TERM and sending myself QUIT instead'
-    Process.kill 'QUIT', Process.pid
-  end
+# Unicorn PID file location
+# pid "/path/to/pids/unicorn.pid"
+pid "/homes/groot/apps/jdbd/pids/unicorn.pid"
 
-  defined?(ActiveRecord::Base) and
-    ActiveRecord::Base.connection.disconnect!
-end
+# Path to logs
+# stderr_path "/path/to/log/unicorn.log"
+# stdout_path "/path/to/log/unicorn.log"
+stderr_path "/homes/groot/apps/jdbd/log/unicorn.log"
+stdout_path "/homes/groot/apps/jdbd/log/unicorn.log"
 
-after_fork do |server, worker|
-  Signal.trap 'TERM' do
-    puts 'Unicorn worker intercepting TERM and doing nothing. Wait for master to send QUIT'
-  end
+# Unicorn socket
+listen "/tmp/unicorn.[app name].sock"
+listen "/tmp/unicorn.myapp.sock"
 
-  defined?(ActiveRecord::Base) and
-    ActiveRecord::Base.establish_connection
-end
+# Number of processes
+# worker_processes 4
+worker_processes 2
+
+# Time-out
+timeout 30
