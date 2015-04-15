fca38107eaf969ff1f359db726e571ab595078fa
Josh Dean <jdbdean@gmail.com>
2015-04-15 19:50:17 +1200

    no rails apps

--- app/views/articles/show.json.jbuilder
+++
@@ -1,1 +0,0 @@
-json.extract! @article, :id, :title, :body, :media_id, :created_at, :updated_at
--- db/seeds.rb
+++
@@ -1,5 +0,0 @@
-email = 'jdbdean@gmail.com'
-if User.where(email: email).blank?
-  generated_password = Devise.friendly_token.first(8)
-  user = User.create!(:email => email, :password => generated_password)
-end
