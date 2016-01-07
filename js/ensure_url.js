20d4f5699bdc6dedf22976511ed097dbd460e9f1
Josh Dean <jdbdean@gmail.com>
2016-01-08 11:00:56 +1300

    allow anyo url

--- js/ensure_url.js
+++ js/ensure_url.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", function(){
-  if(window.location.host != 'jdbd.nz' && window.location.host != 'localhost:4000'){
+  if(window.location.host != 'jdbd.nz' && !window.location.host.includes('localhost:')){
     window.location = "http://jdbd.nz"+window.location.pathname+window.location.hash
   }
 });
